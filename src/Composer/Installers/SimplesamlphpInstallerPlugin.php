1a937ff6fb81453a277fcb44afea249bc20ea392
Luis González <luis.gonzalez@codeenigma.com>
2022-08-04 11:59:36 +0200

    Print some debug info

--- src/Composer/Installers/SimplesamlphpInstallerPlugin.php
+++ src/Composer/Installers/SimplesamlphpInstallerPlugin.php
@@ -8,10 +8,12 @@ use Composer\Package\PackageInterface;
 class SimplesamlphpInstallerPlugin extends LibraryInstaller {
 
   public function supports(string $packageType) {
+    print($packageType);
     return 'simplesamlphp-theme' == $packageType;
   }
 
   public function getInstallPath(PackageInterface $package) {
+    print('vendor/simplesamlphp/simplesamlphp/modules' . $package->getPrettyName());
     return 'vendor/simplesamlphp/simplesamlphp/modules' . $package->getPrettyName();
   }
 
