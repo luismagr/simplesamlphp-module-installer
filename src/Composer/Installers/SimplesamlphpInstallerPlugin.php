3a43ee0add71e20d745fa67504504965ea71d1dd
Luis González <luis.gonzalez@codeenigma.com>
2022-08-04 15:53:48 +0200

    Fix comparasion

--- src/Composer/Installers/SimplesamlphpInstallerPlugin.php
+++ src/Composer/Installers/SimplesamlphpInstallerPlugin.php
@@ -9,7 +9,7 @@ class SimplesamlphpInstallerPlugin extends LibraryInstaller {
 
   public function supports(string $packageType) {
     print($packageType);
-    return 'simplesamlphp-module' == $packageType;
+    return 'simplesamlphp-module' === $packageType;
   }
 
   public function getInstallPath(PackageInterface $package) {
