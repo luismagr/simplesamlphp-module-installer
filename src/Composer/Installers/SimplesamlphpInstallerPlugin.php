2d8d03d15f86038427c4c270eb3ce1aa05704c15
Luis González <luis.gonzalez@codeenigma.com>
2022-08-04 16:02:45 +0200

    Remove debug lines

--- src/Composer/Installers/SimplesamlphpInstallerPlugin.php
+++ src/Composer/Installers/SimplesamlphpInstallerPlugin.php
@@ -8,12 +8,10 @@ use Composer\Package\PackageInterface;
 class SimplesamlphpInstallerPlugin extends LibraryInstaller {
 
   public function supports(string $packageType) {
-    print($packageType);
     return 'simplesamlphp-module' === $packageType;
   }
 
   public function getInstallPath(PackageInterface $package) {
-    print('vendor/simplesamlphp/simplesamlphp/modules' . $package->getPrettyName());
     return 'vendor/simplesamlphp/simplesamlphp/modules' . $package->getPrettyName();
   }
 
